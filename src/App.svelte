280d9eeb85d866a7df730e762ce012446a9cf0e8
VexdStudios <36055611+Zaxiify@users.noreply.github.com>
2022-10-02 23:27:30 -0500

    first commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,53 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from './assets/svelte.svg';
+  import Counter from './lib/Counter.svelte';
+
+  const openSellix = () => {
+    window.open("https://pornhub.com");
+  }
+
+  const openShowcase = () => {
+    window.open("https://www.youtube.com/watch?v=LZ3khexIxhw");
+  }
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1 class="rainbow">neptune</h1>
+  <hr />
+  <button class="button" on:click={openSellix}>purchase</button>
+  <button class="button" on:click={openShowcase}>showcase</button>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  * {
+    font-family: monospace;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  .button {
+    background-color: transparent;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  .rainbow {
+    text-align: center;
+    font-family: monospace;
+    letter-spacing: -1px;
+    animation: colorRotate 6s linear 0s infinite;
   }
-  .read-the-docs {
-    color: #888;
+
+  @keyframes colorRotate {
+    from {
+      color: #6666ff;
+    }
+    10% {
+      color: #0099ff;
+    }
+    50% {
+      color: #00ff00;
+    }
+    75% {
+      color: #ff3399;
+    }
+    100% {
+      color: #6666ff;
+    }
   }
 </style>
